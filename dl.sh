536117bd66271437a5405e6028f5db52bd4e9e23
arhitektura <52233147+arhitektura@users.noreply.github.com>
2019-06-26 09:06:47 +0200

    Add files via upload

---
+++ dl.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/arhitekture/AR19/master/sse.asm
+wget https://raw.githubusercontent.com/arhitekture/AR19/master/mmx.asm
+wget https://raw.githubusercontent.com/arhitekture/AR19/master/muv.asm
+wget https://raw.githubusercontent.com/arhitekture/AR19/master/sort.asm
+wget https://raw.githubusercontent.com/arhitekture/AR19/master/sort10.asm
+wget https://raw.githubusercontent.com/arhitekture/AR19/master/heming.asm
+wget https://raw.githubusercontent.com/arhitekture/AR19/master/tribr.asm
+wget https://raw.githubusercontent.com/arhitekture/AR19/master/fib.asm
